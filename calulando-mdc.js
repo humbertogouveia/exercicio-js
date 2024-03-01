ddc22a362850a80d69cd5f6522270fffa08db847
Humberto Gouveia <64939939+humbertogouveia@users.noreply.github.com>
2024-03-01 19:50:48 -0300

    Delete calulando-mdc.js

--- calulando-mdc.js
+++
@@ -1,7 +0,0 @@
-const calculandoMDC = (primeiroNumero,segundoNumero)=>{
-    while (segundoNumero !==0) {
-        const resto = primeiroNumero % segundoNumero
-    }
-    return console.log(primeiroNumero)
-}
-
